6ea0fecf23ef8c8e5e111edba1b85232318c9767
Techdemo <fleuren.marcel@gmail.com>
2021-08-21 01:11:08 +0200

    added react-query and react-query-devtools. Also an example of fetching an api is added

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
 
 import App from './App';
 
+const queryClient = new QueryClient()
+
 const Root = () => {
   return (
-    <React.StrictMode>  
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>  
   </React.StrictMode>
   )
 }
