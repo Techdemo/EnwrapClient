b13a06172146bac7f5e31385162b07a67f6f1c0b
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-08-21 22:19:21 +0200

    added values and store in dataArray

--- src/components/pages/FormPage/index.jsx
+++ src/components/pages/FormPage/index.jsx
@@ -3,7 +3,7 @@ import { useForm, useFieldArray } from 'react-hook-form';
 
 const FormPage = () => {
   const { control, register, handleSubmit } = useForm();
-  const { fields, append } = useFieldArray({
+  const { fields, append, remove } = useFieldArray({
     control,
     name: "dataArray",
   });
@@ -16,16 +16,44 @@ const FormPage = () => {
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
       {fields.map((field, index) => (
-        <input
-          key={field.id}
-          {...register(`test.${index}.value`)} 
-          defaultValue={field.value}
-        />
+        <div key={field.id}>
+          <input
+            name="title"
+            {...register(`dataArray.${index}.title`)} 
+            defaultValue={field.value}
+            placeholder="Titel"
+          />
+          <input
+            {...register(`dataArray.${index}.description`)} 
+            defaultValue={field.value}
+            placeholder="Omschrijving"
+          />
+          <input
+            {...register(`dataArray.${index}.price`)} 
+            defaultValue={field.value}
+            placeholder="Bedrag"
+          />
+          <input
+            {...register(`dataArray.${index}.amount`)} 
+            defaultValue={field.value}
+            placeholder="Hoeveelheid"
+          />
+          <select
+            {...register(`dataArray.${index}.btw`)}
+          >
+            <option value="" selected="selected" disabled defaultValue={field.value}>Selecteer BTW</option>
+            <option value="0">0%</option>
+            <option value="9">9%</option>
+            <option value="21">21%</option>
+          </select>
+          <button type="button" onClick={() => remove(index)}>Remove</button>
+        </div>
       ))}
         <input type="submit" />
         <button type="button" onClick={() => append({})}>
           Add product
         </button>
+        <button type="button" onClick={() => remove()}>Remove All</button>
       </form>
     </>
   );
