4fa8ba9ece69a7896d501ed81920ba545e434f93
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-08-21 17:59:49 +0200

    added extra lines

--- src/components/pages/Form/index.jsx
+++ src/components/pages/Form/index.jsx
@@ -10,14 +10,19 @@ const Form = () => {
 
 
     return ( 
-        <form onSubmit={handleSubmit(console.log(""))}>
+        <form onSubmit={handleSubmit(console.log)}>
             {fields.map(({ id }, index) => {
                 return (
                     <div key={id}>
-                        <input ref={register()} name={`items[${index}].name`} />
-                        <input ref={register()} name={`items[${index}].description`} />
-                        <select ref={register()} name={`items[${index}].btw`} />
-                        <input ref={register()} type="number" name={`items[${index}].amount`}  />
+                        <input ref={register()} name={`items[${index}].name`} placeholder="title" />
+                        <input ref={register()} name={`items[${index}].description`} placeholder="description" />
+                        <input ref={register()} type="number" name={`items[${index}].price`} placeholder="price"  />
+                        <input ref={register()} type="number" name={`items[${index}].amount`} placeholder="amount"  />
+                        <select ref={register()} name={`items[${index}].btw`} placeholder="btw" >
+                            <option value="0">0%</option>
+                            <option value="9">9%</option>
+                            <option value="21">21%</option>
+                        </select>
                         <button type="button" onClick={() => remove(index)}>Remove</button>
                     </div>
                 )
@@ -25,6 +30,7 @@ const Form = () => {
             <button type="button" onClick={() => append({})}>
                 Append
             </button>
+            <button type="submit">Submit</button>
         </form>
     )
 }
