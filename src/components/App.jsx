8d55353f00a22ebe70b1515f2641d70bab8e2532
Daniel van der Schuur <danielvdschuur@gmail.com>
2021-08-21 15:20:16 +0200

    added themeprovider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { lazy, Suspense } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './modules/theme/theme'
 
 const Prime = lazy(() => import('./modules/Prime'));
 const ExampleForm = lazy(() => import('./modules/ExampleForm'));
@@ -8,13 +10,15 @@ const Login = lazy(() => import('./pages/Login'))
 const App = () => {
   return (
     <main>
-      <Suspense fallback={<span>loading</span>}>
-        <Switch>
-          <Route path="/" component={Prime} exact />
-          <Route path="/form" component={ExampleForm} exact />
-          <Route path="/login" component={Login} exact />
-        </Switch>
-      </Suspense>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={<span>loading</span>}>
+          <Switch>
+            <Route path="/" component={Prime} exact />
+            <Route path="/form" component={ExampleForm} exact />
+            <Route path="/login" component={Login} exact />
+          </Switch>
+        </Suspense>
+      </ThemeProvider>
     </main>
   )
 }
